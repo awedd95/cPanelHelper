2645bb611feab354660831a1c49fb8564e79c68e
awedd95 <awedd95@gmail.com>
2020-05-21 22:46:44 +1000

    Added dns records

--- cPanel.py
+++ cPanel.py
@@ -5,18 +5,22 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 import functools
 import time
+from selenium.webdriver.support.ui import Select
 import tkinter as tk
 import json
 
 class cPanel():
     
-    with open("usable.json") as json_file:
+    with open("converted.json") as json_file:
         data = json.load(json_file)
 
     def init(self):
         global browser
-        browser = Firefox()
+        options = Options()
+        options.headless = True
+        browser = Firefox(options=options)
         browser.implicitly_wait(10)
+
         cPanelSite = 'https://cp1.cloudable.net.au:2083'
         browser.get(cPanelSite)
 
@@ -119,30 +123,109 @@ class cPanel():
         if type == "16":
             return "TXT"
 
+    def remove(self, node):
+        time.sleep(1)
+        node.find_element_by_css_selector("tbody tr td.action-buttons button:nth-child(2)").click()
+        browser.find_element_by_id("modalContinueBtn").click()
+        time.sleep(3)
+
+    def clearOld(self):
+        print("Started clearing")
+        Types = ['CNAME', 'MX', 'A']
+        table =  browser.find_element_by_xpath("//*[@id='table']")
+        rows = table.find_elements(By.TAG_NAME, "tr")
+        td = {}
+        time.sleep(1)
+        for row in rows:
+            item = row.find_elements(By.TAG_NAME, "td[data-title='Type']")
+            if item !=[]:
+                if item[0].text != "TXT":
+                    td[rows.index(row)] = item[0].text
+        print(table, rows, td)
+        if len(set(td.values()).intersection(Types))>0:
+            Test = td.popitem()
+            if Test[1] in Types:
+                print("removing " + Test[1])
+                self.remove(rows[Test[0]])
+                time.sleep(1)
+                done = self.clearOld()
+                return done
+        if len(set(td.values()).intersection(Types)) == 0:
+            print("Finished removing records")
+            return True
+   
+    def addNewRecords(self, item):
+        cleared = False
+        print("Clearing defaults")
+        cleared = self.clearOld()
+        itemRecords = self.data[item] 
+        while not cleared:
+            time.sleep(1)    
+        addRecordButton = browser.find_element_by_id("search_add_record_btn")
+        for key in itemRecords:
+            if "IPAddress" in key:
+                print("Adding" + itemRecords["IPAddress"])
+                addRecordButton.click()
+                submitRecord = browser.find_element_by_id("inline_add_record_button")
+                typeDropDown = Select(browser.find_element_by_id("recordType"))
+
+                browser.find_element_by_id("recordName").send_keys("www")
+                browser.find_element_by_id("record_a_address").send_keys(itemRecords["IPAddress"])
+                submitRecord.click()
+                time.sleep(1)
+
+            if "Exchange" in key:
+                for mx in itemRecords["Exchange"]:
+                    time.sleep(2)
+                    addRecordButton.click()
+                    print("clicked")
+                    time.sleep(2)
+                    submitRecord = browser.find_element_by_id("inline_add_record_button")
+                    typeDropDown = Select(browser.find_element_by_id("recordType"))
+                    print("Adding " + mx)
+                    typeDropDown.select_by_value('MX')
+                    browser.find_element_by_id("record_priority_mx").send_keys(itemRecords["Preference"][itemRecords["Exchange"].index(mx)])
+                    browser.find_element_by_id("record_exchanger").send_keys(mx)
+                    submitRecord.click()
+                    time.sleep(1)
+                    
+            if "text" in key:
+                for txt in itemRecords["text"]:
+                    time.sleep(2)
+                    addRecordButton.click()
+                    time.sleep(2)
+                    submitRecord = browser.find_element_by_id("inline_add_record_button")
+                    typeDropDown = Select(browser.find_element_by_id("recordType"))
+                    print("Adding " + txt)
+                    typeDropDown.select_by_value('TXT')
+                    browser.find_element_by_id("recordName").send_keys(item)
+                    browser.find_element_by_id("record_txtdata").send_keys(itemRecords["text"][itemRecords["text"].index(txt)])
+                    submitRecord.click()
+                    time.sleep(1)
+
+        browser.execute_script("window.history.go(-1)")
+        print("we made it")
 
     def addDNSRecord(self):
         self.wait()
-        filterBox = WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.ID,'filterList_input')))
-        filterBox = browser.find_element_by_id("filterList_input")
         for item in self.data:
-            data = self.printRecords(item)
-            filterBox.send_keys(data['domain'])
-            print(filterBox.get_attribute('value')
-#           if data['domain'] in filterBox.text:
-#               manage = browser.find_element_by_css_selector("tbody tr td.action-buttons button:nth-child(5)")
-#               manage.click()
-            self.wait()
-            addRecordButton = browser.find_element_by_id("search_add_record_btn")
-            recordName = browser.find_element_by_id("recordName")
-            recordType = browser.find_element_by_id("recordType")
-#           recordValue = browser.find_element_by_class("record_subelement subelement_input")
-            self.wait()
-#           addRecordButton.click()
-#           recordName.send_keys("test")
-#           recordValue.send_keys("test")
-#           recordType.select_by_value("AAAA")
-
-        
+            filterBox = WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.ID,'filterList_input')))
+            filterBox = browser.find_element_by_id("filterList_input")
+            filterBox.send_keys(item)
+            time.sleep(0.5)
+            if item in filterBox.get_attribute('value'):
+                manage = browser.find_element_by_css_selector("tbody tr td.action-buttons button:nth-child(5)")
+                addressExists = (manage != None)
+                if addressExists:
+                    manage.click()
+                    time.sleep(2)  
+                    print("Adding " + item)
+                    self.addNewRecords(item)
+                else:
+                    print(item + " not found")
+                    with open('toBeAdded.txt', 'a+') as f:
+                            f.write(item)
+                    filterBox.clear()
     
 class Gui(tk.Frame):
     sites = []
@@ -174,8 +257,7 @@ class Gui(tk.Frame):
         self.page.goToDNS() 
         #for site in self.sites: 
         self.page.addDNSRecord()
-    def testRecords(self):
-        self.page.printRecords()
+
 
     def create_widgets(self):
         self.addDomains = tk.Button(self, text="Add Addon Domains", command=self.addDomainsGui)
@@ -183,9 +265,6 @@ class Gui(tk.Frame):
         
         self.addRecords = tk.Button(self, text="Add DNS Records", command=self.addDNSGui)
         self.addRecords.pack(side="top")
-        
-        self.testRecords = tk.Button(self, text="Test DNS Records", command=self.testRecords)
-        self.testRecords.pack(side="top")
 
         self.quit = tk.Button(self, text="QUIT", fg="red",command=self.master.destroy)
         self.quit.pack(side="bottom")
